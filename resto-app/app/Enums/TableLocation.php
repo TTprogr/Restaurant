b4efff54ac7995e2d229fe0a6f3315cc2c85d809
TTprogr <tenglertomas@gmail.com>
2024-03-16 12:38:03 +0100

    resto

---
+++ resto-app/app/Enums/TableLocation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+/*enum TableLotacion: string
+{
+    case Front = 'front';
+    case Inside = 'inside';
+    case Outside = 'outside';
+}
+*/
---
+++ resto-app/resources/views/thankyou.blade.php
@@ -0,0 +1,6 @@
+<x-guest-layout>
+    <div class="container w-full px-5 py-6 mx-auto">
+        <h1>Tank You</h1>
+        <p>Your Reservation is Ready</p>
+    </div>
+</x-guest-layout>
